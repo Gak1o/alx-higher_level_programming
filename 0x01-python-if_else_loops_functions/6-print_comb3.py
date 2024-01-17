2a899363ff5c8bc63edee51d22dfed6d5ecac1f1
Gak1o <gakiotoni@gmail.com>
2024-01-17 01:14:53 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 out_string = "{:02d},{:02d}"
 for i in range(1, 10):
     for j in range(i+1, 10):
-        for k in range(j+1,10):
-            if i != j and j != k and i !=k:
-        print(out_string.format(i, j), end=", " if j < 9 else "\n")
+        for k in range(j+1, 10):
+            if i != j and j != k and i != k:
+                print(out_string.format(i, j), end=", " if j < 9 else "\n")
