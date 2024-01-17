1e749ea97620c84c388beb305123c9127747fa3b
Gak1o <gakiotoni@gmail.com>
2024-01-17 05:10:48 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-out_string = "{:02d},{:02d}"
-for i in range(1, 10):
-    for j in range(i+1, 10):
-        print(out_string.format(i, j), end=", " if j < 9 else "\n")
+for i in range(10):
+    for j in range(i + 1, 10):
+        if j < 9 or (j == 9 and i < 8):
+            print("{:d}{:d},".format(i, j), end="")
+        elif j == 9 and i == 8:
+            print("{:d}{:d}".format(i, j))
+        if j < 9 or (j == 9 and i < 8):
+            print(" ", end="")
